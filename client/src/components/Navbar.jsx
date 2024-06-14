644a72cdebb6d22e787cca68111ac151f58c9b76
ATN <contact.atn35@gmail.com>
2024-06-14 09:37:32 +0200

    added route editProfil to added main and navbar link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,10 +17,19 @@ export default function Navbar() {
           <NavLink to="/profile"> mon profil </NavLink>
         </li>
         <li>
-          <NavLink to="/profile/edit"> éditer mon profil</NavLink>
+          <NavLink to="/profile/editProfile"> éditer mon profil</NavLink>
         </li>
         <li>
           <NavLink to="/profile/addvehicle"> ajouter un véhicule </NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile/editVehicule"> éditer mon véhicule </NavLink>
+        </li>
+        <li>
+          <NavLink to="/news"> je suis dans actus </NavLink>
+        </li>
+        <li>
+          <NavLink to="/news/:id"> je suis dans infos </NavLink>
         </li>
       </ul>
     </nav>
