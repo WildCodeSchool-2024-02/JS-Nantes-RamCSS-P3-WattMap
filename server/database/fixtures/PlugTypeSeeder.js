1d66b9a606bc73d3b985a680e20b05af35f34527
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-06-19 11:28:24 +0200

    fixed plug insertion, i can now see the 5 plug types in the database.

--- server/database/fixtures/PlugTypeSeeder.js
+++ server/database/fixtures/PlugTypeSeeder.js
@@ -11,13 +11,11 @@ class PlugTypeSeeder extends AbstractSeeder {
   run() {
 
     // Generate and insert plug types data into table 'plug_type'
-    this.insert(
-      { type: "type 1", img_url: '../assets/images/plugTypeIcons/type1.svg', max_power: 100 },
-      { type: "type 2", img_url: '../assets/images/plugTypeIcons/type2.svg', max_power: 100 },
-      { type: "chademo", img_url: '../assets/images/plugTypeIcons/combocss.svg', max_power: 100 },
-      { type: "combo CSS", img_url: '../assets/images/plugTypeIcons/chademo.svg', max_power: 100 },
-      { type: "Tesla", img_url: '../assets/images/plugTypeIcons/tesla.svg', max_power: 100 },
-    );
+    this.insert({ type: "type 1", img_url: '../assets/images/plugTypeIcons/type1.svg', max_power: 100 });
+    this.insert({ type: "type 2", img_url: '../assets/images/plugTypeIcons/type2.svg', max_power: 100 });
+    this.insert({ type: "chademo", img_url: '../assets/images/plugTypeIcons/combocss.svg', max_power: 100 });
+    this.insert({ type: "combo CSS", img_url: '../assets/images/plugTypeIcons/chademo.svg', max_power: 100 });
+    this.insert({ type: "Tesla", img_url: '../assets/images/plugTypeIcons/tesla.svg', max_power: 100 });
   }
 }
 
