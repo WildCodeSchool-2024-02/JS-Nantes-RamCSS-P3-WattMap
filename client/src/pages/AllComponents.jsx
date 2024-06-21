e0930cfd710ec4976908ee7d1dadb7704fc95163
MartinFest <martin.berthaud@laposte.net>
2024-06-21 09:55:36 +0200

    creation composant  Switchbtn

--- client/src/pages/AllComponents.jsx
+++ client/src/pages/AllComponents.jsx
@@ -2,13 +2,14 @@ import "../styles/globals.css";
 import CardNews from "../components/CardNews";
 import CardStation from "../components/CardStation";
 import PlugInfos from "../components/PlugInfos";
+import SwitchBtn from "../components/SwitchBtn";
 
 export default function AllComponents() {
   return (
     <main className="container">
       <h2>CardNews</h2>
       <CardNews />
-
+      <SwitchBtn />
       <h2>CardStation</h2>
       <CardStation />
 
