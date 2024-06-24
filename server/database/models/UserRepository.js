a40bdac562615479c704fd501e42c84622d30cd3
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-06-24 16:07:49 +0200

    added UserRepository

---
+++ server/database/models/UserRepository.js
@@ -0,0 +1,24 @@
+const AbstractRepository = require("./AbstractRepository");
+
+class UserRepository extends AbstractRepository {
+  constructor() {
+    // Call the constructor of the parent class (AbstractRepository)
+    // and pass the table name "item" as configuration
+    super({ table: "user" });
+  }
+
+  // Create operation
+
+  async create({pseudo, firstname, lastname, password, email}) {
+    // the date is calculated right before the insertion
+    const [result] = await this.database.query(
+      `insert into ${this.table} (pseudo, firstname, lastname, password, email, is_admin ) values (?, ?, ?, ?, ?)`,
+      [pseudo, firstname, lastname, password, email, 0]
+    );
+
+    // Return the ID of the newly inserted item
+    return result.insertId;
+  }
+}
+
+module.exports = UserRepository;
