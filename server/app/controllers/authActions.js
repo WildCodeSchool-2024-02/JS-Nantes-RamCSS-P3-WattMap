ccd39c62adbfdcaf3dfc02d1e99e2609a3f19e24
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-06-25 16:31:49 +0200

    added support for cookies in auth process, JWT is included inside of the returned cookie

--- server/app/controllers/authActions.js
+++ server/app/controllers/authActions.js
@@ -1,17 +1,16 @@
 const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
-// const Cookies = require('cookies');
+const Cookies = require('cookies');
 const tables = require("../../database/tables");
 
-
 const verifyPassword = async (password, hashedPassword) => {
-    try {
-      return await argon2.verify(hashedPassword, password);
-    } catch (err) {
-      console.error(err);
-      return false;
-    }
-  };
+  try {
+    return await argon2.verify(hashedPassword, password);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
 
 const login = async (req, res, next) => {
   try {
@@ -38,13 +37,19 @@ const login = async (req, res, next) => {
 
     delete user.password;
 
-    if (token) res.status(200).send({ token, user });
-    else throw new Error("Token not created");
+    // COOKIE credentials
+    const cookie = new Cookies(req, res);
+    cookie.set("token", token, {
+      httpOnly: true,
+      secure: false,
+      expires: new Date(Date.now() + 4 * 60 * 60 * 1000),
+    });
+
+    res.status(200).send({ user });
   } catch (err) {
     next(err);
   }
 };
-
 
 module.exports = {
   login,
