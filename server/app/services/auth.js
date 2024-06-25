c88db591e0ce8e829779496eaf2dcdd8e2b6276c
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-06-25 16:01:13 +0200

    added sample user in the seeder

--- server/app/services/auth.js
+++ server/app/services/auth.js
@@ -58,5 +58,6 @@ const verifyToken = (req, res, next) => {
 
 module.exports = {
   hashPassword,
+  hashingOptions,
   verifyToken
 };
--- server/database/fixtures/UserSeeder.js
+++ server/database/fixtures/UserSeeder.js
@@ -1,4 +1,8 @@
+const argon2 = require("argon2");
 const AbstractSeeder = require("./AbstractSeeder");
+const { hashingOptions } = require("../../app/services/auth"); 
+
+
 
 class UserSeeder extends AbstractSeeder {
   constructor() {
@@ -8,7 +12,7 @@ class UserSeeder extends AbstractSeeder {
 
   // The run method - Populate the 'user' table with fake data
 
-  run() {
+  async run() {
     // Generate and insert fake data into the 'user' table
     for (let i = 0; i < 10; i += 1) {
       // Generate fake user data
@@ -26,6 +30,40 @@ class UserSeeder extends AbstractSeeder {
       // Insert the fakeUser data into the 'user' table
       this.insert(fakeUser); // insert into user(email, password) values (?, ?)
     }
+
+    const adminPsk = await argon2.hash("Capybara49", hashingOptions)
+
+    const sampleAdmin = {
+      pseudo: "Admin", // Generate a fake display name
+      firstname: this.faker.person.firstName(),
+      lastname: this.faker.person.lastName(),
+      email: "admin@example.com", // Generate a fake email using faker library
+      postcode: parseInt(this.faker.location.zipCode(),10), // the ,10 refers to base 10.
+      city: this.faker.location.city(),
+      password: adminPsk, // Generate a fake password using faker library
+      birthdate: this.faker.date.birthdate(),
+      is_admin: 1
+    };
+
+    // Insert the fakeUser data into the 'user' table
+    this.insert(sampleAdmin); 
+
+
+    const sampleUser = {
+      pseudo: "User", // Generate a fake display name
+      firstname: this.faker.person.firstName(),
+      lastname: this.faker.person.lastName(),
+      email: "user@example.com", // Generate a fake email using faker library
+      postcode: parseInt(this.faker.location.zipCode(),10), // the ,10 refers to base 10.
+      city: this.faker.location.city(),
+      password: adminPsk, // Generate a fake password using faker library
+      birthdate: this.faker.date.birthdate(),
+      is_admin: 0
+    };
+
+    // Insert the fakeUser data into the 'user' table
+    this.insert(sampleUser); 
+
   }
 }
 
