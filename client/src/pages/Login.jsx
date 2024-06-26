60024bdb73ce59a60db2d1aa6fa423ae9f56e2e4
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-06-26 17:25:36 +0200

    login is redirected to /map if successful

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,23 @@
-import { useRef } from "react";
+import { useRef, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import Input from "../components/Input";
 
 export default function Login() {
+
+  // refs are used in order to not trigger a re-render everytime the content of inputs change
   const emailRef = useRef();
   const passwordRef = useRef();
 
+  // used to give feedback to the user when logging in
+  const [isLogged,setIsLogged] = useState(false);
+  const [isPending,setIsPending] = useState(false);
+
+  // used to automatically navigate to a page
+  const navigate = useNavigate();
+
+
   const handleFetch = async (data) => {
+    setIsPending(true)
     const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
       method: "POST",
       headers: {
@@ -15,11 +27,15 @@ export default function Login() {
       body: JSON.stringify(data),
     });
 
+    setIsPending(false);
+
     if (response.ok) {
       const res = await response.json();
-      // localStorage.setItem("token", res.token);
       console.info("Logged", res);
+      setIsLogged(true);
+      setTimeout(()=>navigate("/map"),1500);
     }
+    
   };
 
   const handleSubmit = async (event) => {
@@ -42,16 +58,18 @@ export default function Login() {
 
   return (
     <main className="container">
-      <h1>se connecter</h1>
-      <form onSubmit={handleSubmit}>
+      <h1>Connexion</h1>
+      {!isPending?(<form onSubmit={handleSubmit}>
         <Input type="text" labelText="Email" reference={emailRef} />
         <Input
           type="password"
           labelText="Mot de passe"
           reference={passwordRef}
         />
-        <button type="submit">login</button>
-      </form>
+        {isLogged&&(<p>Connexion réussie</p>)}
+        <button type="submit" disabled = {isPending}>login</button>
+      </form>):(<h2>pending</h2>)}
+      
     </main>
   );
 }
