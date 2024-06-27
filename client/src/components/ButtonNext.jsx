7d275a031decd28b7c53a5fe2d957fd85066bcc9
MartinFest <martin.berthaud@laposte.net>
2024-06-27 17:14:37 +0200

    animation css de la fleche

--- client/src/components/ButtonNext.jsx
+++ client/src/components/ButtonNext.jsx
@@ -7,11 +7,12 @@ export default function ButtonNext({ anchorID, text, icon }) {
   return (
     <Link
       to={`#${anchorID}`}
-      className="btn-next"
+          className="btn-next"
       aria-label={`Section ${text}`}
     >
-      <span className="libelle">{text}</span>
-      <Icons choiceIcon={icon} />
+          <span className="libelle">{text}</span>
+          <span className="bounce"><Icons choiceIcon={icon} /></span>
+      
     </Link>
   );
 }
