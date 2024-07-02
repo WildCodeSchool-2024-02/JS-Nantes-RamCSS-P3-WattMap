8b3bd06f47312526f52bb6006dbdf46e0bee3d36
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-02 10:00:00 +0200

    installed multer and created route /upload/profilepicture

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -11,8 +11,11 @@ const plugTypesRouter = require("./plugTypes/router");
 const stationsRouter = require("./stations/router");
 const articlesRouter = require("./articles/router");
 const usersRouter = require("./users/router");
+const uploadRouter = require("./uploads/router");
+
 const { login } = require("../../controllers/authActions");
 
+// public routers
 router.use("/items", itemsRouter);
 router.use("/plugtypes", plugTypesRouter);
 router.use("/stations", stationsRouter);
@@ -21,6 +24,9 @@ router.use("/users", usersRouter);
 
 // route for user login
 router.post("/login", login)
+
+// TODO : put authentication wall here
+router.use("/upload", uploadRouter);
 
 /* ************************************************************************* */
 
---
+++ server/app/routers/api/uploads/router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+// const path = require("path");
+
+// const uploadsFolderPath = path.join(__dirname, "../../../../public/uploads");
+
+// const multer = require("multer");
+
+
+// Route to get a list of items
+router.post("/profilepicture", ()=> console.info("router ok"));
+// TODO router.patch -> update user profile
+
+
+/* ************************************************************************* */
+
+module.exports = router;
