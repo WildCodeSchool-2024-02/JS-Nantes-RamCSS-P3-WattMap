eecf7113d6a2aa708620c60b0841cffc9bc42c63
MartinFest <martin.berthaud@laposte.net>
2024-07-02 15:29:37 +0200

    add ProfileImage component and css

---
+++ client/src/components/ProfileImage.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+// import Icons from "./Icons";
+
+
+export default function ProfileImage() {
+    // const [isProfileImageCustom, SetIsProfileImageCustom] = useState();
+//     return (
+//         // <figure className={`profile-image${isProfileImageCustom ? ' custom-profile-image' : ''}`}>
+//         //     {isProfileImageCustom ? <Icons choiceIcon="user" /> : <img src="" alt="" />}
+//         //     <figcaption>{isProfileImageCustom ? "Avatar par défaut" : "Nom perso"}</figcaption>
+//         // </figure>
+//     )
+}
