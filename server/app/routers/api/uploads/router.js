25c58c9f7a1c50853e9508767c76f6669873979d
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-02 10:09:14 +0200

    first upload ok

--- server/app/routers/api/uploads/router.js
+++ server/app/routers/api/uploads/router.js
@@ -2,17 +2,58 @@ const express = require("express");
 
 const router = express.Router();
 
-// const path = require("path");
+const path = require("path");
 
-// const uploadsFolderPath = path.join(__dirname, "../../../../public/uploads");
+const uploadsFolderPath = path.join(__dirname, "../../../../public/assets/images/profilePictures");
 
-// const multer = require("multer");
+const multer = require("multer");
 
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, uploadsFolderPath);
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${path.parse(file.originalname).name}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+// check the uploaded file type for security reasons
+const upload = multer({
+  storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid mime type"));
+    }
+    return null;
+  },
+});
 
 // Route to get a list of items
-router.post("/profilepicture", ()=> console.info("router ok"));
-// TODO router.patch -> update user profile
-
+router.post("/profilepicture", upload.single("file"), (req, res) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: "Please send file" });
+      }
+      
+      const { filename } = req.file; 
+  
+      return res.json({ message: "File uploaded !", filename });
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  });
 
 /* ************************************************************************* */
 
