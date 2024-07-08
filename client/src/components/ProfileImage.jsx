ab9da92521a496f58aa3b9173205d448692db6fd
MartinFest <martin.berthaud@laposte.net>
2024-07-08 16:28:17 +0200

    add new folder /assets/icons with icon  pen.svg and style css for ProfileImage.jsx

--- client/src/components/ProfileImage.jsx
+++ client/src/components/ProfileImage.jsx
@@ -1,13 +1,14 @@
 // import { useState } from "react";
 // import Icons from "./Icons";
+import "../styles/profileImage.css"
 
 
 export default function ProfileImage() {
     // const [isProfileImageCustom, SetIsProfileImageCustom] = useState();
-//     return (
-//         // <figure className={`profile-image${isProfileImageCustom ? ' custom-profile-image' : ''}`}>
-//         //     {isProfileImageCustom ? <Icons choiceIcon="user" /> : <img src="" alt="" />}
-//         //     <figcaption>{isProfileImageCustom ? "Avatar par défaut" : "Nom perso"}</figcaption>
-//         // </figure>
-//     )
+    // return (
+    //     <figure className={`profile-image${isProfileImageCustom ? ' custom-profile-image' : ''}`}>
+    //         {isProfileImageCustom ? < img src="" alt="" /> : <Icons choiceIcon="user" />}
+    //         <figcaption className="visually-hidden">{isProfileImageCustom ? "Avatar par défaut" : "Nom perso"}</figcaption>
+    //     </figure>
+    // )
 }
