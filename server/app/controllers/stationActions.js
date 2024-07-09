5b2275a4cf4bc145d6fb9f21f7b052336e428b3e
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-09 16:53:13 +0200

    added readByAddress method in station reposiory, and also addMany in the station actions

--- server/app/controllers/stationActions.js
+++ server/app/controllers/stationActions.js
@@ -1,3 +1,6 @@
+// Import CSV and file reading utilities
+const fs = require("fs");
+const csv = require("csv-parser");
 // Import access to database tables
 const tables = require("../../database/tables");
 
@@ -15,7 +18,59 @@ const browse = async (req, res, next) => {
   }
 };
 
+// this function iterates through a CSV to upload new stations
+const addMany = async (req, res, next) => {
+  try {
+    // there should be a middleware that uploads the file and gets its path, for now we'll just wirte it down
+    const filePath = "./database/Stations2.csv";
+    // this is a stream that will read the file line by line
+    const stream = fs.createReadStream(filePath).pipe(csv());
+    stream
+      .on("data", async (row) => {
+        // for every line in the csv file
+
+        // pause stream until treatment of this line is finished.
+        stream.pause();
+        try {
+          // before inserting we need to make sure that the station isn't already in db
+          // Fetch stations with the name name in db
+          const existingStation = await tables.station.readByAddress(
+            row.adresse_station
+          );
+          console.info(existingStation);
+          // if the station doesn't exist already :
+          if (existingStation.length === 0) {
+            const newStation = await tables.station.create({
+              latitude: row.consolidated_longitude,
+              longitude: row.consolidated_latitude,
+              name: row.nom_station,
+              address: row.adresse_station,
+              price: 10,
+              maxPower: 250,
+              imgUrl: "/public/assets/stations/sample.jpg",
+            });
+            console.info("inserted", newStation);
+          } else {
+            console.info("Station already exist, skipping");
+          }
+          // resume stream - treat next line
+          stream.resume();
+        } catch (err) {
+          console.info(err);
+        }
+      })
+      .on("end", () => {
+        // Respond with the stations in JSON format
+        res.status(200).json("CSV file was successfully processed");
+      });
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
 // Ready to export the controller functions
 module.exports = {
-  browse
+  browse,
+  addMany,
 };
--- server/database/models/ReservationRepository.js
+++ server/database/models/ReservationRepository.js
@@ -16,6 +16,17 @@ class ReservationRepository extends AbstractRepository {
     // Return the array of items
     return rows;
   }
+
+  async readByAddress(search) {
+    // Execute the SQL SELECT query to retrieve stations with the queried address "search" 
+    const [row] = await this.database.query(
+      `select id, name, address, latitude, longitude,  price, max_power as maxPower, img_url as imgUrl from ${this.table} where address=?`,
+      [search]
+    );
+
+    // Return the array of stations
+    return row;
+  }
 }
 
 module.exports = ReservationRepository;
