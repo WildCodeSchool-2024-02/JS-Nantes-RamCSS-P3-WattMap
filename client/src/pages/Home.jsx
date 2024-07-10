a0e4e35e7eb24c97c007b70c52aba1275694b787
MartinFest <martin.berthaud@laposte.net>
2024-07-10 16:26:00 +0200

    modify CardCta component adding multiples props and put it on homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,42 @@
+import CardCta from "../components/CardCta";
+
 export default function Home() {
-  return <h1>je suis dans la homepage</h1>;
+  return (
+    <main className="container">
+      <h1>je suis dans la homepage</h1>
+
+      <section className="section-home">
+        <CardCta
+          cardLight={false}
+          imageUrl="map-with-pin.png"
+          title="Trouvez votre borne de recharge"
+          description="Lorem ipsum dolor sit amet consectetur. Ultrices tincidunt pellentesque"
+          iconButton="map"
+          labelButton="voir la carte"
+        />
+      </section>
+
+      <section className="section-home">
+        <CardCta
+          cardLight
+          imageUrl="booking-plugstation-calendar-example.png"
+          title="Réservez votre borne facilement"
+          description="Plus d’attente aux stations : réserver en avance votre borne."
+          iconButton="clock"
+          labelButton="réservez"
+        />
+      </section>
+
+      <section className="section-home">
+        <CardCta
+          cardLight={false}
+          imageUrl="finger-roadmap.jpeg"
+          title="Créez votre compte"
+          description="Lorem ipsum dolor sit amet consectetur. Ultrices tincidunt pellentesque "
+          iconButton="user"
+          labelButton="S'inscrire"
+        />
+      </section>
+    </main>
+  );
 }
