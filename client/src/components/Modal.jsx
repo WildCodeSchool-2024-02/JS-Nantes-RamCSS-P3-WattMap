7bf083d64dbdf62adae1aed4733dab58fbc05bf4
MartinFest <martin.berthaud@laposte.net>
2024-07-10 12:24:53 +0200

    add button close and style to Modal.jsx + add class to the body tag  when modal is open, to add a colored background under the modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,20 +1,25 @@
+import PropTypes from "prop-types";
 import Input from "./Input";
 
-export default function Modal() {
+export default function Modal({ closeModal }) {
     // function close() {
     //     console.log("close");
     // }
 
     return (
-        <dialog aria-labelledby="title_dialog" >
-            <h1 id="title_dialog">Ajouter votre photo</h1>
-            <form id='personForm' >
+        <dialog className="modal" aria-labelledby="title_dialog" >
+            <h2 className="title-modal" id="title_dialog">Ajouter votre photo</h2>
+            <form id='personForm' className="form" >
                 <Input type="file" labelText="Photo" />
-                <div className="d-md-flex justify-content-md-between">
-                    <button type="button">Annuler</button>
-                    <button type="submit">Envoyer</button>
+                <div className="d-md-flex justify-content-md-between mt-2">
+                    <button onClick={closeModal} type="button" className="btn btn-disable">Annuler</button>
+                    <button type="submit" className="btn btn-default">Envoyer</button>
                 </div>
             </form>
         </dialog>
     );
 }
+
+Modal.propTypes = {
+    closeModal: PropTypes.func.isRequired,
+};
--- client/src/pages/EditProfile.jsx
+++ client/src/pages/EditProfile.jsx
@@ -3,6 +3,7 @@ import ProfileImage from "../components/ProfileImage";
 import Signup from "../components/Signup";
 
 export default function EditProfile() {
+
   return (
     <main className="container">
       <h1>je suis dans éditer mon profil</h1>
