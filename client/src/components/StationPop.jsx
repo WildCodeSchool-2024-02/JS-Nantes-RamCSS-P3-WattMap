7effbb72b6d18a2f833e2804fdbfebb95fd1054c
Lucasa49 <lucasaksu@gmail.com>
2024-07-10 15:44:27 +0200

    refactored map page and inserted CardStation

--- client/src/components/StationPop.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/statPop.css";
-
-export default function StationPop() {
-  return (
-    <div className="card-map">
-      <p className="station-name">oui</p>
-    </div>
-  );
-}
--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,11 +1,28 @@
-import MapInsert from "../components/MapInsert";
-import StationPop from "../components/StationPop";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import CardStation from "../components/CardStation";
+import "../styles/map.css";
 
 export default function Map() {
   return (
     <main>
-      <MapInsert />
-      <StationPop />
+      <MapContainer
+        style={{ height: "100vh", width: "100vw", position: "relative" }}
+        center={[48.85897, 2.29324]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <div id="map" />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[48.85897, 2.29324]}>
+          <Popup>
+            ici les frere <br /> tour effeil
+          </Popup>
+        </Marker>
+      </MapContainer>
+      <CardStation />
     </main>
   );
 }
