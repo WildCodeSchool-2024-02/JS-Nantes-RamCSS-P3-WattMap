c686579e95260fa8833125e093425d33da68550c
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-11 21:02:51 +0200

    added a function that wan generate week dates in order to map datePickers on them

--- client/src/pages/Station.jsx
+++ client/src/pages/Station.jsx
@@ -2,15 +2,28 @@ import { useLoaderData } from "react-router-dom";
 import CardStation from "../components/CardStation";
 import DatePicker from "../components/DatePicker";
 
-
 function Station() {
   const station = useLoaderData();
+
+  // This function will generate an array of 7 dates for the upcoming week
+  function generateWeekDates() {
+    const dates = [];
+    const today = new Date();
+    Array(7).forEach( i => {
+      const date = new Date(today);
+      date.setDate(today.getDate() + i);
+      dates.push(date);
+    })
+    return dates;
+  }
 
   return (
     <main className="p-3">
       <h1>Réservation</h1>
-      <CardStation displayMode={1} station={station}/>
-      <DatePicker />
+      <CardStation displayMode={1} station={station} />
+      {generateWeekDates().map((date) => (
+        <DatePicker key={date} day={date} />
+      ))}
     </main>
   );
 }
