bc35020a98c094a15bae5446d82d4fc70404a9cf
MartinFest <martin.berthaud@laposte.net>
2024-07-12 11:35:03 +0200

    add Edit.jsx  component and put it on EditProfile page

--- client/src/pages/EditProfile.jsx
+++ client/src/pages/EditProfile.jsx
@@ -1,14 +1,20 @@
-import "../styles/form.css";
+// import { useLoaderData } from "react-router-dom";
 import ProfileImage from "../components/ProfileImage";
-import Signup from "../components/Signup";
+import Edit from "../components/Edit";
+import "../styles/form.css";
 
 export default function EditProfile() {
+  // const profileUSer = useLoaderData();
+  // console.log('%c⧭', 'color: #00e600', profileUSer);
 
   return (
     <main className="container">
       <h1>je suis dans éditer mon profil</h1>
-      <ProfileImage />
-      <Signup />
+      {/* {profileUSer.map((profile) => (
+     ))} */}
+      <ProfileImage displayMode={1} />
+      <Edit />
     </main>
   );
+
 }
