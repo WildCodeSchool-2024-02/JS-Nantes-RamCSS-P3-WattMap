35160f9c764c67fe90a482ba37e3bb73f94c6184
ATN <contact.atn35@gmail.com>
2024-07-22 22:46:49 +0200

    the composite part of the image page is almost finished, really almost finished

--- client/src/components/VehicleImage.jsx
+++ client/src/components/VehicleImage.jsx
@@ -1,20 +1,36 @@
 import PropTypes from "prop-types";
 import Icons from "./Icons";
-import "../styles/vehicleImage.css";
+import "../styles/profileImage.css";
 
-export default function VehicleImage({ imgUrl }) {
+export default function VehicleImage({ isEditable = false, imgUrl }) {
   return (
-    <div className="vehicle-image">
-      {imgUrl ? <img src={imgUrl} alt="" /> : <Icons choiceIcon="car" />}
-    </div>
+    <figure className={`profile-image ${isEditable ? "edit-car-image" : ""}`}>
+      {imgUrl ? (
+        <img
+          src={`${import.meta.env.VITE_API_URL}${imgUrl}`}
+          alt="Votre véhicule"
+          className="vehicle-image"
+        />
+      ) : (
+        <>
+          <Icons choiceIcon="car" />
+          <figcaption className="visually-hidden">
+            Image par défaut
+          </figcaption>
+        </>
+      )}
+    </figure>
   );
 }
 
+// Déclaration des propTypes
 VehicleImage.propTypes = {
   imgUrl: PropTypes.string,
+  isEditable: PropTypes.bool
 };
 
+// Déclaration des defaultProps
 VehicleImage.defaultProps = {
-  imgUrl: 'http://www.w3.org/2000/svg',
+  isEditable: false,
+  imgUrl: ""
 };
-
