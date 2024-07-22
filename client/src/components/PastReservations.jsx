69ba0e64c07c835bc832563f0d9296a6d77fad69
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-22 10:42:17 +0200

    fixed proptypes console warnings, li keys and missing images in the Station seeder

--- client/src/components/PastReservations.jsx
+++ client/src/components/PastReservations.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import CardStation from "./CardStation";
 
-export default function PastReservations({ reservations }) {
+export default function PastReservations({ reservations = [] }) {
 
   return (
     <section>
@@ -9,21 +9,22 @@ export default function PastReservations({ reservations }) {
         <ul>
           {reservations.map((reservation) => {
             // this part is used to convert date into a readable string
-            const date=new Date(reservation.reservationDate)
-            const dateString=`${date.toLocaleDateString('fr')} - ${date.toLocaleTimeString(['fr'], { hour: "2-digit", minute: "2-digit" })}`
-            return(
-            <li key={reservation.stationId} className="list-unstyled">
-              <CardStation
-                displayMode={0}
-                station={{
-                  name: reservation.stationName,
-                  address: reservation.stationAddress,
-                  imgUrl: reservation.stationImgUrl,
-                }}
-                date={dateString}
-              />
-            </li>
-          )})}
+            const date = new Date(reservation.reservationDate);
+            const dateString = `${date.toLocaleDateString("fr")} - ${date.toLocaleTimeString(["fr"], { hour: "2-digit", minute: "2-digit" })}`;
+            return (
+              <li key={reservation.reservationDate+reservation.stationId} className="list-unstyled">
+                <CardStation
+                  displayMode={0}
+                  station={{
+                    name: reservation.stationName,
+                    address: reservation.stationAddress,
+                    imgUrl: reservation.stationImgUrl,
+                  }}
+                  date={dateString}
+                />
+              </li>
+            );
+          })}
         </ul>
       ) : (
         "Aucune réservation dans l'historique, voir la carte ?"
@@ -35,5 +36,14 @@ export default function PastReservations({ reservations }) {
 // linter was disabled because of default props soon to be deprecated
 /* eslint-disable react/require-default-props */
 PastReservations.propTypes = {
-  reservations: PropTypes.string,
+  reservations: PropTypes.arrayOf(
+    PropTypes.shape({
+      reservationDate: PropTypes.string.isRequired,
+      duration: PropTypes.number.isRequired,
+      stationId: PropTypes.number.isRequired,
+      stationAddress: PropTypes.string.isRequired,
+      stationImgUrl: PropTypes.string.isRequired,
+      stationName: PropTypes.string.isRequired,
+    })
+  ),
 };
