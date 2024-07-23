d62d781a73898ec32c578c40c45e3e09bc240cb3
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-23 11:17:43 +0200

    a user can now make a reservation, added POST method to the modal

--- client/src/components/ConfirmReservationModal.jsx
+++ client/src/components/ConfirmReservationModal.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export default function ConfirmReservationModal({ dateTime, closeModal }) {
+  const [feedback, setFeedback] = useState("");
+
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/reservations`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ stationId: 3, date: dateTime, duration: 30 }),
+      }
+    );
+
+    if (response.ok) {
+      setFeedback("✅ Réservation confirmée !");
+      setTimeout(closeModal, 2000);
+      // reload the page to see the changes, we can't perform a hard refresh since we're in an SPA so we'll navigate to the page we're already in
+      setTimeout(() => navigate("/bookings"), 2200);
+    } else {
+      setFeedback("❌ Erreur dans la réservation.");
+    }
   };
 
   // used to display the date in a human readable way
@@ -38,6 +64,7 @@ export default function ConfirmReservationModal({ dateTime, closeModal }) {
             CONFIRMER
           </button>
         </section>
+        <p>{feedback}</p>
       </form>
     </dialog>
   );
@@ -45,5 +72,5 @@ export default function ConfirmReservationModal({ dateTime, closeModal }) {
 
 ConfirmReservationModal.propTypes = {
   closeModal: PropTypes.func.isRequired,
-  dateTime: PropTypes.instanceOf(Date).isRequired
+  dateTime: PropTypes.instanceOf(Date).isRequired,
 };
