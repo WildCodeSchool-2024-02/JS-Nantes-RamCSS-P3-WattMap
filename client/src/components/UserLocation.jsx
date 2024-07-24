608e96f2f93fec827b33bc1899420cce5d60cbd9
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-24 09:59:23 +0200

    added a modal to ask the user wether he wants to activate geolocation or not

--- client/src/components/UserLocation.jsx
+++ client/src/components/UserLocation.jsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Popup, Marker, useMap } from "react-leaflet";
 import { usePosition } from "../contexts/PositionProvider";
 
 export default function UserLocation() {
   const { userLocation, setUserLocation } = usePosition();
+
+  // This state is used to open and close the modal that asks for geolocation
+  const [modalIsOpen, setModalIsOpen] = useState(true);
 
   function getUserLocation() {
     if (navigator.geolocation) {
@@ -20,7 +23,6 @@ export default function UserLocation() {
     }
   }
 
-  useEffect(getUserLocation, []);
   const map = useMap();
 
   useEffect(() => {
@@ -28,10 +30,36 @@ export default function UserLocation() {
   }, [userLocation]);
 
   return (
-    <Marker position={userLocation}>
-      <Popup>
-        <p>Votre position</p>
-      </Popup>
-    </Marker>
+    <>
+      <Marker position={userLocation}>
+        <Popup>
+          <p>Votre position</p>
+        </Popup>
+      </Marker>
+      {modalIsOpen && (
+        <dialog className="modal">
+            <h2>Partagez votre position pour voir les bornes autour de vous</h2>
+            <section className="w-100 d-md-flex justify-content-md-between mt-2">
+              <button
+                className="btn btn-disable"
+                onClick={() => setModalIsOpen(false)}
+                type="button"
+              >
+                RETOUR
+              </button>
+              <button
+                onClick={() => {
+                  getUserLocation();
+                  setModalIsOpen(false);
+                }}
+                className="btn btn-default"
+                type="button"
+              >
+                ACTIVER LA GEOLOCALISATION
+              </button>
+            </section>
+        </dialog>
+      )}
+    </>
   );
 }
