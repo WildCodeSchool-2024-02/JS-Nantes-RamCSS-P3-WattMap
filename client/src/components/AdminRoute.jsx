be9ae545c8a5f553ab8cb96f593a6c90bc7c2092
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-24 11:09:59 +0200

    tested sucessfully that admin routes CANNOT be accessed by a standard user

--- client/src/components/AdminRoute.jsx
+++ client/src/components/AdminRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useAuth } from "../contexts/AuthProvider";
 
-// This component protects the route that should only be accessible when logged in as a user
+// This component protects the route that should only be accessible when logged in as an admin
 export default function AdminRoute({ children }) {
   const { isAdmin } = useAuth();
 
